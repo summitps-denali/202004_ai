f093642811437279e0598ff24903a0a4536ea583
EC2 Default User <ec2-user@ip-172-31-37-233.us-east-2.compute.internal>
2020-06-02 09:01:04 +0000

    added sample arena bee bots and area bot_a arena compatible class

---
+++ tsa2.js
@@ -0,0 +1,21 @@
+var bot = require("./bot_a");   //  REQUIRED
+
+//  Arena Server - likely these value are obsolete, check for new ones
+bot.hostURL = 'http://do01.zaptech.org:8080';
+bot.key = "g0cnz2nzy";        
+//  Arean Server - end
+
+/*  TEST  */
+bot.isTest = false; //  false means disabled test, run against arena server
+bot.testHostURL = 'http://127.0.0.1:8080';
+bot.testKey = "testrun";
+/*  TEST - end  */
+
+bot.direction = function(game) {
+    //  Write your code in this function!!!
+    var dirs = ["north", "east", "south", "west"];
+    var myDir = dirs[Math.floor(Math.random() * 4)];
+    return myDir;
+}
+
+bot();  //  REQUIRED
